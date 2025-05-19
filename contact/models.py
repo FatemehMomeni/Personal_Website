5c249ce95a11071600d739d8a9871fcfb6766323
mhmojtabaei <mh.mojtabaei96@gmail.com>
2025-05-19 09:56:45 +0330

    Update models.py

--- contact/models.py
+++ contact/models.py
@@ -6,3 +6,7 @@ class React(models.Model):
     msgSubject = models.CharField(max_length=100)
     emailAddress = models.EmailField()
     messageText = models.TextField()
+    class Meta():
+        verbose_name = "message"
+    def __str__(self):
+        return self.msgSubject
