0d8c8c268b627d80e5f2c08309c5eb1aecbc450d
mhmojtabaei <mh.mojtabaei96@gmail.com>
2025-05-19 09:48:52 +0330

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from contact.models import React
 
 # Register your models here.
+
+class ReactAdmin(admin.ModelAdmin):
+    empty_value_display = '-empty-'
+    list_display = ('msgSubject', 'emailAddress', 'messageText')
+    list_filter = ('emailAddress',)
+    search_fields = ['msgSubject', 'emailAddress']
+admin.site.register(React,ReactAdmin)
